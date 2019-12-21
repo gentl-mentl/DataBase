b8aaf53f9f6ef87d153eaf76f7e1d2344d009d78
gentl-mentl <59124806+gentl-mentl@users.noreply.github.com>
2019-12-21 22:57:21 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,7 +12,7 @@ public class Main {
         BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
 
        Group t1=new Group(2);
-       t1.addParson("maks","j",9);
+       t1.addParson("misha","j",9);
         t1.addParson("anton","2",9);
         t1.searchPerson("j");
         t1.deletePerson("maks");
